ce6cbf29325b9354dee3600d2fdb34b693c64e06
zhanghui <794532030@qq.com>
2018-01-06 22:35:48 +0800

    suanf

---
+++ common/src/main/java/com/hui/zhang/algorithm/GenerateParentheses.java
@@ -0,0 +1,32 @@
+package com.hui.zhang.algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by zhanghui on 2018/1/6.
+ */
+public class GenerateParentheses {
+
+
+    public static List<String> generateParentheses(int n) {
+
+        List<String> list = new ArrayList<String>();
+        backtract(list, "", 0, 0, n);
+        return list;
+    }
+
+    public static void backtract(List<String> list, String str, int open, int close, int max) {
+
+        if (str.length() == max * 2) {
+            list.add(str);
+            return;
+        }
+        if (open < max) {
+            backtract(list, str + '(', open + 1, close, max);
+        }
+        if (close < open) {
+            backtract(list, str + ')', open, close + 1, max);
+        }
+    }
+}
