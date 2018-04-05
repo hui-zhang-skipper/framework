c6dc7b23fa3c2375e4d9d23ddca1a326c748083b
zhanghui <794532030@qq.com>
2018-04-05 10:45:55 +0800

    suanf

--- common/src/main/java/com/hui/zhang/leetcode/LC1TwoSum.java
+++ common/src/main/java/com/hui/zhang/leetcode/LC1TwoSum.java
@@ -1,5 +1,7 @@
 package com.hui.zhang.leetcode;
 
+import com.alibaba.fastjson.JSON;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,16 +10,25 @@ import java.util.Map;
  */
 public class LC1TwoSum {
 
-    public int[] twoSum(int[] numbers, int target) {
+    public static int[] twoSum(int[] numbers, int target) {
         int[] result = new int[2];
         Map<Integer, Integer> map = new HashMap<Integer, Integer>();
         int length = numbers.length;
         for (int i = 0; i < length; i++) {
             if (map.containsKey(target - numbers[i])) {
-                result[1] = i + 1;
+                result[1] = i;
                 result[0] = map.get(target - numbers[i]);
+            }
+            else {
+                map.put(numbers[i],i);
             }
         }
         return result;
+    }
+
+    public static void main(String[] args){
+        int[] number=new int[]{2,7,9,12};
+        int[] a=twoSum(number,14);
+        System.out.println(JSON.toJSON(a));
     }
 }
